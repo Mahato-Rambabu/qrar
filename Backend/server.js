abf076f5a0a2fc2a387164a75cfc404587e3f89a
Rambabu Mahato <mahatorambabu114@gmail.com>
2025-01-19 19:36:06 +0530

    url changes

--- Backend/server.js
+++ Backend/server.js
@@ -39,7 +39,7 @@ app.use(
 
 app.use(cookieParser()); 
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // MongoDB Connection
 connect(process.env.MONGO_URI)
