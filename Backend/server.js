6241cd8ee2f453e10d939d8eb95c2f5ba28aa8e0
Rambabu Mahato <mahatorambabu114@gmail.com>
2025-01-20 00:30:50 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -31,7 +31,7 @@ const io = initializeSocket(server);
 
 app.use(
   cors({
-    origin: "https://qrar-lyart.vercel.app", // Allow your Vercel frontend URL
+    origin: "https://qrar-lyart.vercel.app/", // Allow your Vercel frontend URL
     credentials: true, // Allow cookies to be sent
   })
 );
