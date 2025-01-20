471d50046fdf1041dc22461b375e35e749a33f73
Rambabu Mahato <mahatorambabu114@gmail.com>
2025-01-20 23:30:35 +0530

    Update socket.js

--- Backend/sockets/socket.js
+++ Backend/sockets/socket.js
@@ -1,33 +1,25 @@
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
 
 const initializeSocket = (server) => {
     const io = new Server(server, {
-        cors: { 
-            origin: [
-                "https://qrar-lyart.vercel.app",
-                "https://qrar-front-jet.vercel.app"
-            ],
-            methods: ["GET", "POST"],
-            credentials: true 
-        },
-        transports: ['websocket', 'polling'], 
+        cors: { origin: process.env.FRONTEND_BASE_URL, methods: ['GET', 'POST', 'PUT', 'DELETE'] },
     });
 
-    io.on("connection", (socket) => {
-        console.log("User connected:", socket.id);
+    io.on('connection', (socket) => {
+        console.log('User connected:', socket.id);
 
-        socket.on("order:created", (order) => {
-            console.log("New order received:", order);
-            io.emit("order:created", order); 
+        socket.on('order:created', (order) => {
+            console.log('New order:', order);
+            io.emit('order:created', order);
         });
 
-        socket.on("order:statusChanged", (data) => {
-            console.log("Order status changed:", data);
-            io.emit("order:updated", data);
+        socket.on('order:statusChanged', (data) => {
+            console.log('Order status changed:', data);
+            io.emit('order:updated', data);
         });
 
-        socket.on("disconnect", () => {
-            console.log("User disconnected:", socket.id);
+        socket.on('disconnect', () => {
+            console.log('User disconnected:', socket.id);
         });
     });
 
