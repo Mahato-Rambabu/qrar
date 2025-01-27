acd6c263d0fb90dd6920f11b5d177d9e1a8c8a82
Rambabu Mahato <mahatorambabu114@gmail.com>
2025-01-27 21:11:42 +0530

    remove jwt-decode

--- Dashboard/src/utils/auth.js
+++ Dashboard/src/utils/auth.js
@@ -1,18 +1,7 @@
 import Cookies from 'js-cookie';
-import jwt_decode from 'jwt-decode'; // Install with `npm install jwt-decode`
 
 export const isAuthenticated = () => {
   const token = Cookies.get('authToken');
 
   if (!token) return false; // No token means not authenticated
-
-  try {
-    const decoded = jwt_decode(token); // Decode the token
-    const isTokenExpired = decoded.exp * 1000 < Date.now(); // Check expiry
-
-    return !isTokenExpired; // Return false if expired
-  } catch (error) {
-    console.error('Invalid token:', error);
-    return false;
-  }
 };
