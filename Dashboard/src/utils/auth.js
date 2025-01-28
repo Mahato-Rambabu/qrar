7177109f34ef0bffffa08e02d1950b4c917a23d1
Rambabu Mahato <mahatorambabu114@gmail.com>
2025-01-28 10:34:33 +0530

    ji

--- Dashboard/src/utils/auth.js
+++ Dashboard/src/utils/auth.js
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie';
 
 export const isAuthenticated = () => {
-  const token = Cookies.get('authToken');
-  return !!token; // Return true if token exists, false otherwise
+  return document.cookie.includes('authToken'); // ✅ Only checks if token exists
 };
+
