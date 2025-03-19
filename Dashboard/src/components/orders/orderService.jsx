7783138c11b3b884b0274b90a902e7413d810418
Anuj-010 <anujpatwari5555@gmail.com>
2025-03-20 01:53:46 +0530

    A

--- Dashboard/src/components/orders/orderService.jsx
+++ Dashboard/src/components/orders/orderService.jsx
@@ -1,5 +1,3 @@
-import axios from "axios";
-import Cookies from 'js-cookie'; // Import js-cookie
 import axiosInstance from "../../utils/axiosInstance";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
